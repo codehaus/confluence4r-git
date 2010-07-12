735861a756e07ca4fc3fa3ebcb3bea205775eaa8
Ben <ben@walding.com>
2010-07-12 14:59:44 +1000

    example code

---
+++ examples/example-bootstrap.rb
@@ -0,0 +1,11 @@
+################################################################################
+#  Copyright (c) 2004-2010, by OpenXource, LLC. All rights reserved.
+#
+#  THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF OPENXOURCE
+#
+#  The copyright notice above does not evidence any
+#  actual or intended publication of such source code.
+################################################################################
+$: << './lib'
+require 'rubygems'
+gem 'soap4r'
